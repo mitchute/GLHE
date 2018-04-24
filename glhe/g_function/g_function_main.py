771ed8edf551bbed9c0cbf747c044656baa41ab8
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-04-24 17:22:45 -0500

    incremental commit working on testing load aggregation

--- glhe/g_function/g_function_main.py
+++ glhe/g_function/g_function_main.py
@@ -1,12 +1,12 @@
-from loads.bins.no_aggregation_method import NoAggMethod
-from loads.bins.static_method import StaticMethod
 from loads.bins.dynamic_method import DynamicMethod
 from loads.bins.method_types import MethodType
+from loads.bins.no_aggregation_method import NoAggMethod
+from loads.bins.static_method import StaticMethod
 
 
 class GFunction(object):
 
-    def __init__(self, agg_method='DYNAMIC'):
+    def __init__(self, agg_method='DYNAMIC', profile='FIXED'):
 
         self._agg_method_name = agg_method.upper()
 
@@ -18,9 +18,11 @@ class GFunction(object):
             self._agg = StaticMethod()
         elif self._agg_method_name == 'NONE':
             self._agg_type = MethodType.NOAGG
-            self._agg = NoAggMethod
+            self._agg = NoAggMethod()
         else:
             raise ValueError("'{}' is not supported".format(self._agg_method_name))
 
+        self._profile_name = profile.upper()
+
     def simulate(self):
-        self._agg.loads[0].get_load
+        a = self._agg.loads[0].get_load()
---
+++ standalone/run_g_function.py
@@ -0,0 +1,6 @@
+from glhe.g_function.g_function_main import GFunction
+
+
+if __name__ == '__main__':
+
+    GFunction()
