a343a3bf0a687580f34aac6af8706125357488c1
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-09-05 15:54:01 -0500

    setup static runs

--- studies/aggregation/static/setup_static_cases.py
+++ studies/aggregation/static/setup_static_cases.py
@@ -9,7 +9,7 @@ from studies.aggregation.base.write_pbs import write_pbs  # noqa
 
 run_times = [SEC_IN_YEAR, 5 * SEC_IN_YEAR]
 loads = ['balanced', 'imbalanced']
-wall_times = ['1:00:00', '3:00:00']
+wall_times = ['20:00', '1:30:00']
 
 bin_widths = [[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384],
               [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683],
@@ -34,8 +34,8 @@ bin_widths = [[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16
               [1, 23, 529, 12167],
               [1, 24, 576, 13824]]
 
-min_num_bins_hourly = [1, 2, 4, 8, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-min_num_bins_others = 4
+min_num_bins_hourly = [4, 8, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+min_num_bins_others = [4, 6, 8, 10]
 
 # shortcuts
 join = os.path.join
@@ -43,34 +43,47 @@ norm = os.path.normpath
 cwd = os.getcwd()
 
 
-def write_static_json_input(run_dir, time, path_to_load, path_to_g, path_to_output):
+def write_static_json_input(run_dir, time, path_to_load, path_to_g, path_to_output, other):
     f = load_json(norm(join(cwd, '../base', 'base_case.json')))
     f['flow-profile']['external']['path'] = path_to_load
     f['load-profile']['external']['path'] = path_to_load
     f['g-functions']['file'] = path_to_g
     f['simulation']['runtime'] = time
     f['simulation']['output-path'] = path_to_output
+
+    f['load-aggregation']['type'] = 'static'
+    f['load-aggregation']['static']['min number bins'] = other['min number bins']
+    f['load-aggregation']['static']['bin widths in hours'] = other['bin widths in hours']
+
     write_json(join(run_dir, 'in.json'), f)
 
-
-def setup_case():
-    for idx, time in enumerate(run_times):
-        for load in loads:
-            run_name = '{}_{}'.format(load, int(time / SEC_IN_YEAR))
-            run_path = join(cwd, 'runs', run_name)
-            if not os.path.exists(run_path):
-                os.makedirs(run_path)
-            load_path = '{}.csv'.format(norm(join(cwd, '../base', load)))
-            g_path = norm(join(cwd, '../base', 'g_functions.csv'))
-            output_path = join(run_path, 'out.csv')
-            write_static_json_input(run_path, time, load_path, g_path, output_path)
-            write_pbs(run_path, wall_times[idx])
-
-
 def setup_all_cases():
-    for width in bin_widths:
-        for hour in min_num_bins_hourly:
-            pass
+    for idx, load in enumerate(loads):
+        for time in run_times:
+
+            load_time_name = '{}_{}'.format(load, int(time / SEC_IN_YEAR))
+
+            for num_hourly in min_num_bins_hourly:
+                for num_other in min_num_bins_others:
+                    for width in bin_widths:
+
+                        run_name = '{}_{}_{}'.format(num_hourly, num_other, width[1])
+                        run_path = join(cwd, 'runs', load_time_name, run_name)
+
+                        if not os.path.exists(run_path):
+                            os.makedirs(run_path)
+
+                        load_path = '{}.csv'.format(norm(join(cwd, '../base', load)))
+                        g_path = norm(join(cwd, '../base', 'g_functions.csv'))
+                        output_path = join(run_path, 'out.csv')
+
+                        other_inputs = {'bin widths in hours': width,
+                                        'min number bins': [num_other] * len(width)}
+
+                        other_inputs['min number bins'][0] = num_hourly
+
+                        write_static_json_input(run_path, time, load_path, g_path, output_path, other_inputs)
+                        write_pbs(run_path, wall_times[idx])
 
 
 if __name__ == "__main__":
