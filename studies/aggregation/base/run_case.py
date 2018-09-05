630a94533cb0a05036d0fea0cb5e8b5e06ab80b5
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-09-05 09:52:00 -0500

    fix PATH

--- studies/aggregation/base/run_case.py
+++ studies/aggregation/base/run_case.py
@@ -1,13 +1,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../..'))
+sys.path.insert(0, os.path.abspath('../../../../..'))
 
 from standalone.run_g_function import RunGFunctions
 
 if __name__ == "__main__":
-    try:
-        RunGFunctions(sys.argv[1])
-        print("initialized")
-    except:
-        print("fail")
+    RunGFunctions(sys.argv[1]).simulate()
