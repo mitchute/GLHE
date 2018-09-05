e3d958aec670def82e122c75ce4b7ad19b7dafca
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-09-05 10:01:21 -0500

    flake8

--- studies/aggregation/base/run_case.py
+++ studies/aggregation/base/run_case.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../../../../..'))
 
-from standalone.run_g_function import RunGFunctions
+from standalone.run_g_function import RunGFunctions  # noqa
 
 if __name__ == "__main__":
     RunGFunctions(sys.argv[1]).simulate()
