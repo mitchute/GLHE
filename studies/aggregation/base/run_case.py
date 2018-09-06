ff5611b07af05380bbc28715248ab9f10d113c4d
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-09-06 05:19:32 -0500

    change runs scripts for the static cases

--- studies/aggregation/base/run_case.py
+++ studies/aggregation/base/run_case.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../../../..'))
+sys.path.insert(0, os.path.abspath('../../../../../../..'))
 
 from standalone.run_g_function import RunGFunctions  # noqa
 
