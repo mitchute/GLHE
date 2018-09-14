6dd1da77bd29974a459e29e0d9a32b3422077a36
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-09-14 10:09:39 -0500

    fix type

--- studies/aggregation/scripts/run_case.py
+++ studies/aggregation/scripts/run_case.py
@@ -4,7 +4,7 @@ import sys
 
 def _append_path(num_up):
     path_str = ''
-    for i in range(num_up):
+    for i in range(int(num_up)):
         path_str += '../'
     sys.path.insert(0, os.path.abspath(path_str))
 
