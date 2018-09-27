d0b6aba636e8be7146b0aa07e53a985012ea499b
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-09-27 16:16:12 -0500

    adding plotting script

---
+++ studies/aggregation/scripts/make_rmse_vs_time_plots.py
@@ -0,0 +1,36 @@
+import os
+
+import matplotlib.pyplot as plt
+
+# shortcuts
+cwd = os.getcwd()
+norm = os.path.normpath
+join = os.path.join
+
+
+def extract_case_data(path, load, time):
+    path = norm(join(cwd, path))
+
+    return 0, 0
+
+
+def make_plots():
+    loads = ['balanced', 'imbalanced']
+    times = [1, 5]
+
+    runs = {"static": {"name": 'Static',
+                     "path": '../static/runs/static_stats.csv'}}
+
+    for load in loads:
+        for time in times:
+            fig = plt.figure()
+
+            for this_run in sorted(runs):
+                x, y = extract_case_data(runs[this_run]["path"], load, time)
+                fig.plot(x, y, label=runs[this_run]["name"])
+
+            plt.savefig('{}_{}.png'.format(load, time), bbox_inches='tight')
+
+
+if __name__ == "__main__":
+    make_plots()
