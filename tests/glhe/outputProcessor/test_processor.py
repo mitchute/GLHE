084cfdc8fb74ca0854352c6e4b06ad3908467af2
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-10-17 09:02:52 -0500

    coverage

--- tests/glhe/outputProcessor/test_processor.py
+++ tests/glhe/outputProcessor/test_processor.py
@@ -1,4 +1,8 @@
+import os
+import tempfile
 import unittest
+
+import pandas as pd
 
 from glhe.outputProcessor.processor import OutputProcessor
 
@@ -21,10 +25,29 @@ class TestOutputProcessor(unittest.TestCase):
     def report_output(self):
         return {"bar": self.bar}
 
-    def test_report_output(self):
+    def test_collect_output(self):
         d = DummyClass()
 
         self.op.collect_output([d.report_output(), self.report_output()])
 
         self.assertEqual(self.op.df['foo'][0], 1)
         self.assertEqual(self.op.df['bar'][0], 2)
+
+    def test_write_to_file(self):
+        d = DummyClass()
+
+        self.op.collect_output([d.report_output(), self.report_output()])
+
+        temp_dir = tempfile.mkdtemp()
+        temp_file = os.path.join(temp_dir, 'temp_data.csv')
+        with open(temp_file, 'w') as f:
+            f.write('Some, data, here\n')
+
+        self.op.write_to_file(temp_file)
+
+        self.assertTrue(os.path.exists(temp_file))
+
+        df = pd.read_csv(temp_file)
+
+        self.assertEqual(df['foo'].iloc[0], 1)
+        self.assertEqual(df['bar'].iloc[0], 2)
