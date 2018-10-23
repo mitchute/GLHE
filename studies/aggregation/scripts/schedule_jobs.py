accc94ef67a2233418b61b6cf8b97c6e4d8d7b61
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-10-23 11:52:02 -0500

    working on job scheduler

--- studies/aggregation/scripts/schedule_jobs.py
+++ studies/aggregation/scripts/schedule_jobs.py
@@ -1,36 +1,95 @@
+import fnmatch
 import os
 import subprocess
 import sys
+import time
+import pandas as pd
 
 # shortcuts
 join = os.path.join
 
 
-def call_cd(path):
-    print(os.getcwd())
-    subprocess.call(['cd', path])
-    print(os.getcwd())
+def count_running():
+    running = 0
+    idle = 0
+    error = 0
+
+    for line in call_showq():
+        if b'Running' in line:
+            running += 1
+        elif b'Idle' in line:
+            idle += 1
+        else:
+            error += 1
+
+    return running, idle
 
 
-def init_sim():
-    return subprocess.call(['ls', 'run.pbs'])
+def call_showq():
+    return subprocess.Popen(['showq', '|', 'grep', 'mitchute'], stdout=subprocess.PIPE).stdout.read()
 
 
-def call_tail(path):
+def init_sim(path):
+    return subprocess.call(['(cd', path, '&&', 'qsub', 'run.pbs)'])
+
+
+def tail_file(path):
     return subprocess.Popen(['tail', path], stdout=subprocess.PIPE).stdout.read()
 
 
 def find_all_jobs(path):
-    return call_cd(path)
+    jobs = []
 
-    # df = pd.DataFrame(colums)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if fnmatch.fnmatch(file, 'run.pbs'):
+                jobs.append(root)
 
-    # for root, dirs, files in os.walk(path):
+    df = pd.DataFrame(data={'path': jobs,
+                            'completed': [0] * len(jobs),
+                            'errors': [0] * len(jobs)})
+
+    return df
+
+
+def count_completed_jobs_in_dir(path):
+    completed = 0
+    errors = 0
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if fnmatch.fnmatch(file, 'run.pbs.*'):
+                if b'Final runtime:' in tail_file(join(root, file)):
+                    completed += 1
+                else:
+                    errors += 1
+
+    return completed, errors
+
+
+def count_completed_jobs(df):
+    for idx, row in df.iterrows():
+        completed, errors = count_completed_jobs_in_dir(row['path'])
+        df.loc[idx, 'completed'] = completed
+        df.loc[idx, 'errors'] = errors
+
+    return df
 
 
 def schedule_jobs(path, num_runs):
-    tail = find_all_jobs(path)
-    print(tail)
+    df = find_all_jobs(path)
+
+    max_idle = 50
+
+    while True:
+        df = count_completed_jobs(df)
+        running, idle = count_running()
+
+        for _ in range(max_idle - idle):
+            for idx, row in df.iterrows():
+                init_sim(row['path'])
+
+        time.sleep(300)
 
 
 if __name__ == "__main__":
