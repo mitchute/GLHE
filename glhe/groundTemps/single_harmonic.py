9ab23faba6d2392244409abac87cbf34e1a87966
Matt Mitchell <matt.s.mitchell@okstate.edu>
2018-11-04 08:58:02 -0600

    flake8

--- glhe/groundTemps/single_harmonic.py
+++ glhe/groundTemps/single_harmonic.py
@@ -29,6 +29,6 @@ class SingleHarmonic(BaseGroundTemp):
 
     def get_temp(self, time, depth):
         term1 = -depth * sqrt(pi / (SEC_IN_YEAR * self._soil_diffusivity))
-        term2 = (2 * pi / SEC_IN_YEAR) * (time - self._phase_shift - (depth / 2) *
-                                          sqrt(SEC_IN_YEAR / (pi * self._soil_diffusivity)))
+        term2 = (2 * pi / SEC_IN_YEAR) * (
+                    time - self._phase_shift - (depth / 2) * sqrt(SEC_IN_YEAR / (pi * self._soil_diffusivity)))
         return self._ave_ground_temp - self._amplitude * exp(term1) * cos(term2)
