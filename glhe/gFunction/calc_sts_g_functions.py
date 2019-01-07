47b65751bb02b5b4ed2a0ff891392929759297fc
Matt Mitchell <matt.s.mitchell@okstate.edu>
2019-01-07 16:42:16 -0600

    incremental on adding Xu STS g-function calcs

---
+++ glhe/gFunction/calc_sts_g_functions.py
@@ -0,0 +1,13 @@
+class STSGFunctions(object):
+    """
+      X. Xu and Jeffrey D. Spitler. 2006. 'Modeling of Vertical Ground Loop Heat Exchangers
+      with Variable Convective Resistance and Thermal Mass of the Fluid.' in Proceedings of
+      the 10th International Conference on Thermal Energy Storage-EcoStock. Pomona, NJ, May 31-June 2
+    """
+
+    def __init__(self):
+        pass
+
+
+def calc_sts_g_functions(inputs):
+    pass
---
+++ glhe/gFunction/radial_sts_cell.py
@@ -0,0 +1,3 @@
+class RadialCell(object):
+    def __init__(self):
+        pass
