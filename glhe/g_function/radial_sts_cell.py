ca039efb12e2484b5d5eb1baf2811ec0b3380d8f
Matt Mitchell <matt.s.mitchell@okstate.edu>
2019-04-24 16:06:52 -0500

    coverage

--- glhe/g_function/radial_sts_cell.py
+++ glhe/g_function/radial_sts_cell.py
@@ -6,16 +6,16 @@ class RadialCell(object):
     def __init__(self, inputs):
         self.type = inputs['type']
 
-        self.inner_radius = inputs['inner radius']
-        self.center_radius = inputs['center radius']
-        self.outer_radius = inputs['outer radius']
+        self.inner_radius = inputs['inner-radius']
+        self.center_radius = inputs['center-radius']
+        self.outer_radius = inputs['outer-radius']
 
         self.thickness = inputs['thickness']
 
-        self.temperature = inputs['initial temperature']
-        self.prev_temperature = inputs['initial temperature']
+        self.temperature = inputs['initial-temperature']
+        self.prev_temperature = inputs['initial-temperature']
 
         self.conductivity = inputs['conductivity']
-        self.rho_cp = inputs['vol heat capacity']
+        self.rho_cp = inputs['vol-heat-capacity']
 
         self.volume = pi * (self.outer_radius ** 2 - self.inner_radius ** 2)
--- tests/standalone/test_plant_loop.py
+++ tests/standalone/test_plant_loop.py
@@ -1,16 +1,31 @@
-# import os
-# import unittest
-#
-# from standalone.plant_loop import PlantLoop
-#
-#
-# class TestPlantLoop(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.this_file_directory = os.path.dirname(os.path.realpath(__file__))
-#
-#     def test_simulate(self):
-#         json_file_path = os.path.normpath(
-#                 os.path.join(self.this_file_directory, '..', '..', 'glhe', 'examples', 'single.json'))
-#         p = PlantLoop(json_file_path)
-#         self.assertTrue(p.simulate())
+import os
+import tempfile
+import unittest
+
+from glhe.globals.functions import load_json
+from glhe.globals.functions import write_json
+from standalone.plant_loop import PlantLoop
+
+norm = os.path.normpath
+join = os.path.join
+
+
+class TestPlantLoop(unittest.TestCase):
+
+    def setUp(self):
+        self.this_file_directory = os.path.dirname(os.path.realpath(__file__))
+
+    def test_simulate(self):
+        temp_dir = tempfile.mkdtemp()
+        temp_file = join(temp_dir, 'in.json')
+        input_path = norm(join(self.this_file_directory, '..', '..', 'test_files', 'single.json'))
+        d = load_json(input_path)
+
+        g_path = norm(join(self.this_file_directory, '..', '..', 'validation', 'MFRTRT_EWT_g_functions',
+                           'EWT_experimental_g_functions.csv'))
+
+        d['ground-heat-exchanger'][0]['g-function-path'] = g_path
+        d['simulation']['output-path'] = temp_dir
+        write_json(temp_file, d)
+        p = PlantLoop(temp_file)
+        self.assertTrue(p.simulate())
