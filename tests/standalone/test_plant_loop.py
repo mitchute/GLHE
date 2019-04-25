933c806445660ddc38f82d9ae9e51e47024ba467
Matt Mitchell <matt.s.mitchell@okstate.edu>
2019-04-24 20:07:39 -0500

    Travis debugging

--- tests/standalone/test_plant_loop.py
+++ tests/standalone/test_plant_loop.py
@@ -16,12 +16,14 @@ class TestPlantLoop(unittest.TestCase):
         self.this_file_directory = os.path.dirname(os.path.realpath(__file__))
 
     def test_simulate(self):
+        print("TESTPLANTLOOP DIR: {}".format(self.this_file_directory))
         temp_dir = tempfile.mkdtemp()
         temp_file = join(temp_dir, 'in.json')
         input_path = norm(join(self.this_file_directory, '..', '..', 'test_files', 'single.json'))
         d = load_json(input_path)
         g_path = norm(join(self.this_file_directory, '..', '..', 'test_files', 'single_g_functions.csv'))
         d['ground-heat-exchanger'][0]['g-function-path'] = g_path
+        print("GFUNCTIONPATH {}".format(g_path))
         d['simulation']['output-path'] = temp_dir
         write_json(temp_file, d)
         p = PlantLoop(temp_file)
